fd828bf821f12362e537f6b0bcc1fd0d36722f7b
JasperZ <jasper.android@gmail.com>
2016-09-30 14:05:07 +0200

    commented data_logger package

--- src/data_logger/DataEntry.java
+++ src/data_logger/DataEntry.java
@@ -6,54 +6,106 @@ public class DataEntry {
 	private double threshold;
 	private int correctPredictions;
 	private int wrongPredictions;
-	private int wrongButPredictionCandidates;
 	private int noPredictions;
 	private int triedPredictions;
 	private int totalPredictions;
 
-	public DataEntry(double threshold, int correctPredictions, int wrongPredictions, int wrongButPredictionCandidates,
-			int noPredictions, int triedPredictions, int totalPredictions) {
+	/**
+	 * Creates a entry for the DataLogger
+	 * 
+	 * @param threshold
+	 *            Threshold of this entry
+	 * @param correctPredictions
+	 *            Number of correct predictions
+	 * @param wrongPredictions
+	 *            Number of wrong predictions
+	 * @param noPredictions
+	 *            Number of not done prediction
+	 * @param triedPredictions
+	 *            Number of tried predictions
+	 * @param totalPredictions
+	 *            Number of all predictions, including tried and not tried
+	 *            predictions
+	 */
+	public DataEntry(double threshold, int correctPredictions, int wrongPredictions, int noPredictions,
+			int triedPredictions, int totalPredictions) {
 		this.threshold = threshold;
 		this.correctPredictions = correctPredictions;
 		this.wrongPredictions = wrongPredictions;
-		this.wrongButPredictionCandidates = wrongButPredictionCandidates;
 		this.noPredictions = noPredictions;
 		this.triedPredictions = triedPredictions;
 		this.totalPredictions = totalPredictions;
 	}
 
+	/**
+	 * Returns the threshold of this entry
+	 * 
+	 * @return Threshold
+	 */
 	public double getThreshold() {
 		return threshold;
 	}
 
+	/**
+	 * Returns the number of correct predictions
+	 * 
+	 * @return Number of correct predictions
+	 */
 	public int getCorrectPredictions() {
 		return correctPredictions;
 	}
 
+	/**
+	 * Returns the number of wrong predictions
+	 * 
+	 * @return Number of wrong predictions
+	 */
 	public int getWrongPredictions() {
 		return wrongPredictions;
 	}
 
-	public int getWrongButPredictionCandidates() {
-		return wrongButPredictionCandidates;
-	}
-
+	/**
+	 * Returns the number of not done predictions
+	 * 
+	 * @return Number of not done predictions
+	 */
 	public int getNoPredictions() {
 		return noPredictions;
 	}
 
+	/**
+	 * Returns the number of tried predictions
+	 * 
+	 * @return Number of tried predictions
+	 */
 	public int getTriedPredictions() {
 		return triedPredictions;
 	}
 
+	/**
+	 * Returns a string version of this entry with information about prediction
+	 * rate and accuracy
+	 * 
+	 * @return String of the format: "threshold, prediction rate, accuracy"
+	 */
 	public String toString(int maxTriedPredictions) {
 		return String.format(Locale.ENGLISH, "%f,%f,%f", threshold, predictionRate(), accuracy());
 	}
 
+	/**
+	 * returns the accuracy of this entry
+	 * 
+	 * @return Accuracy
+	 */
 	public double accuracy() {
 		return (double) correctPredictions / (double) triedPredictions;
 	}
 
+	/**
+	 * Returns the prediction rate of this entry
+	 * 
+	 * @return Prediction Rate
+	 */
 	public double predictionRate() {
 		return (double) triedPredictions / (double) totalPredictions;
 	}
--- src/data_logger/DataLogger.java
+++ src/data_logger/DataLogger.java
@@ -8,24 +8,44 @@ import java.util.Locale;
 
 import org.apache.commons.io.FileUtils;
 
+/**
+ * A data logger to log the results of the predictions with different thresholds
+ * 
+ * @author jasper
+ *
+ */
 public class DataLogger {
 	private final static String FILE_HEAD = "threshold,predictionRate,accuracy";
 	private ArrayList<DataEntry> entries;
 
+	/**
+	 * Creates a new data logger without any entries
+	 */
 	public DataLogger() {
 		entries = new ArrayList<>();
 	}
 
+	/**
+	 * Adds an entry to this logger
+	 * 
+	 * @param entry
+	 *            Entry to add
+	 */
 	public void newEntry(DataEntry entry) {
 		entries.add(entry);
 	}
 
+	/**
+	 * Writes the current logging session to a given path
+	 * 
+	 * @param path
+	 *            Path where the file will be stored
+	 */
 	public void writeRelative(String path) {
 		String outputBuffer = FILE_HEAD;
-		int maxCounter = maxCounter();
 
 		for (DataEntry entry : entries) {
-			outputBuffer += String.format(Locale.ENGLISH, "\n%s", entry.toString(maxCounter));
+			outputBuffer += String.format(Locale.ENGLISH, "\n%s", entry.toString());
 		}
 
 		try {
@@ -33,21 +53,5 @@ public class DataLogger {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-	}
-
-	public int maxCounter() {
-		int result = 0;
-
-		for (DataEntry entry : entries) {
-			if (entry.getCorrectPredictions() > result) {
-				result = entry.getCorrectPredictions();
-			} else if (entry.getWrongPredictions() > result) {
-				result = entry.getWrongPredictions();
-			} else if (entry.getWrongButPredictionCandidates() > result) {
-				result = entry.getWrongButPredictionCandidates();
-			}
-		}
-
-		return result;
 	}
 }
